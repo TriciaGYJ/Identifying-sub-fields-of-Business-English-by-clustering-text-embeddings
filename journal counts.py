a91e287508dc6ffe75f8052dd8f03858da42e101
Triciagu <108021207+TriciaGYJ@users.noreply.github.com>
2023-05-02 16:32:52 -0400

    Code for bibliometric analysis on journals

---
+++ journal counts.py
@@ -0,0 +1,26 @@
+import json
+with open("OpenAlex_BusinessEnglish_070722.json", "r") as f:
+    dat = json.load(f)
+    #print(dat)
+author_count = dict()
+for item in dat:
+    journal = item.get("host_venue", {}).get("display_name",None)
+
+    if journal is None:
+        continue
+    else:
+              if journal in author_count:
+                  author_count[journal] += 1
+              else:
+                  author_count[journal] = 1
+
+    count=sorted(author_count.items(), key=lambda x: x[1], reverse=True)
+
+    print(count)
+
+
+
+
+
+
+
